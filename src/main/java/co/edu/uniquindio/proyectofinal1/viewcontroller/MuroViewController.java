5a319761ee0bbe8e3afa4835f6d65da7e67d2c35
Alilopez12 <allisonlopez1928@gmail.com>
2024-10-26 16:30:49 -0500

    interfaz muro

--- src/main/java/co/edu/uniquindio/proyectofinal1/viewcontroller/MuroViewController.java
+++ src/main/java/co/edu/uniquindio/proyectofinal1/viewcontroller/MuroViewController.java
@@ -1,4 +1,40 @@
 package co.edu.uniquindio.proyectofinal1.viewcontroller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MuroViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnChats;
+
+    @FXML
+    private Button btnVerMas;
+
+    @FXML
+    void OnChats(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnVerMas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnChats != null : "fx:id=\"btnChats\" was not injected: check your FXML file 'muro-view.fxml'.";
+        assert btnVerMas != null : "fx:id=\"btnVerMas\" was not injected: check your FXML file 'muro-view.fxml'.";
+
+    }
+
 }
