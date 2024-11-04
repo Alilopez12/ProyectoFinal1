a47f4d71ea245158c92d39eaf9d0ca52b4051595
David Salazar <davidsalazar@MacBook-Pro-de-David.local>
2024-11-04 15:43:36 -0500

    logica botin enter, cambios interfaces

--- src/main/java/co/edu/uniquindio/proyectofinal1/viewcontroller/LoginViewController.java
+++ src/main/java/co/edu/uniquindio/proyectofinal1/viewcontroller/LoginViewController.java
@@ -1,10 +1,17 @@
 package co.edu.uniquindio.proyectofinal1.viewcontroller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import co.edu.uniquindio.proyectofinal1.MarketPlaceApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginViewController {
 
@@ -28,6 +35,17 @@ public class LoginViewController {
 
     @FXML
     private TextField txtIngresarUsuario;
+
+
+    @FXML
+    void OnEnter(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MarketPlaceApplication.class.getResource("ingresar-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Registro!");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     void initialize() {
