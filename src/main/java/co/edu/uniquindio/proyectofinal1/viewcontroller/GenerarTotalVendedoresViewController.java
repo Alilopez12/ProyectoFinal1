56d402a95d0ae88d8c632ae23c318a97c7b8955e
DanielValenciaArenas <159465752+DanielValenciaArenas@users.noreply.github.com>
2024-11-19 15:52:01 -0500

    GeneracionTotalVendedores

--- src/main/java/co/edu/uniquindio/proyectofinal1/viewcontroller/GenerarTotalVendedoresViewController.java
+++ src/main/java/co/edu/uniquindio/proyectofinal1/viewcontroller/GenerarTotalVendedoresViewController.java
@@ -1,6 +1,31 @@
 package co.edu.uniquindio.proyectofinal1.viewcontroller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class GenerarTotalVendedoresViewController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void OnBackTotalVendedores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnBack != null : "fx:id=\"btnBack\" was not injected: check your FXML file 'GenerarTotalVendedores-view.fxml'.";
+
+    }
 
 }
