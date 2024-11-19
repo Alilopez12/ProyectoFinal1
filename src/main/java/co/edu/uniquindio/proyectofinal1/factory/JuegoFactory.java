1cd8f6e8c096860787c77e1c54a25184c2e82d48
DanielValenciaArenas <159465752+DanielValenciaArenas@users.noreply.github.com>
2024-11-19 10:06:13 -0500

    Abstract Factory, Logica ventanas Login, logica comentarios

---
+++ src/main/java/co/edu/uniquindio/proyectofinal1/factory/JuegoFactory.java
@@ -0,0 +1,21 @@
+package co.edu.uniquindio.proyectofinal1.factory;
+
+import co.edu.uniquindio.proyectofinal1.model.EstadoProducto;
+import co.edu.uniquindio.proyectofinal1.model.Producto;
+import co.edu.uniquindio.proyectofinal1.service.IProductoFactory;
+
+import java.util.Collections;
+
+public class HogarFactory implements IProductoFactory {
+
+    public Producto crearProducto(String nombre, String idProducto, String imagen, double precio, EstadoProducto estado) {
+        return Producto.builder()
+                .nombre(nombre)
+                .idProducto(idProducto)
+                .imagen(imagen)
+                .categoria("Hogar")
+                .precio(precio)
+                .estadoProducto(estado)
+                .build();
+    }
+}
---
+++ src/main/java/co/edu/uniquindio/proyectofinal1/service/IProductoFactory.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.proyectofinal1.service;
+
+public interface IProductoFactory {
+}
