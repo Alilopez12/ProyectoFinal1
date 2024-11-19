fe580c59165002f082ab4c5d320523adbf189cee
Alilopez12 <allisonlopez1928@gmail.com>
2024-11-19 16:07:50 -0500

    interfaces

--- src/main/java/co/edu/uniquindio/proyectofinal1/viewcontroller/OlvidoContraseñaViewController.java
+++ src/main/java/co/edu/uniquindio/proyectofinal1/viewcontroller/OlvidoContraseñaViewController.java
@@ -1,4 +1,40 @@
 package co.edu.uniquindio.proyectofinal1.viewcontroller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class OlvidoContraseñaViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    void OnBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnEnviar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnBack != null : "fx:id=\"btnBack\" was not injected: check your FXML file 'olvidoContraseña-view.fxml'.";
+        assert btnEnviar != null : "fx:id=\"btnEnviar\" was not injected: check your FXML file 'olvidoContraseña-view.fxml'.";
+
+    }
+
 }
